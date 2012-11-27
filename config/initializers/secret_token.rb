c29da234702871d53b325406e0229aa069b74726
Your Name <phillipchen@gmail.com>
2012-11-26 22:42:32 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plusone::Application.config.secret_token = '5f6aa2426da3040c838fa5a847f03ce4e53c292511ca7b92baf4a0ea16c344263b535a57109cde3b289fedb4ad92eb5934b3d9d4b3da4f2dcd9a81fa89391872'
